32f5e37dca00f368a1a344febf53e476a1d1ed11
Aryan <aryanpranshu@gmail.com>
2019-02-04 19:27:14 +0530

    Dashboard Updated

--- Faculty_Information_System/Form1.vb
+++ Faculty_Information_System/Form1.vb
@@ -8,19 +8,28 @@ Public Class Form1
     Public Property EmailPass As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim query As String = "Select Name From faculty_info where Email= '" & EmailPass & "';"
+        Dim name As String
+        Dim dept As String
+        Dim email As String
+        Dim query As String = "SELECT * FROM faculty_info where Email= '" & EmailPass & "';"
         Dim conn = New OleDbConnection(connectionString)
+        conn.Open()
         Dim cmd As New OleDbCommand(query, conn)
+        Dim Reader As OleDbDataReader = cmd.ExecuteReader()
         Try
-            conn.Open()
-            Dim name As String = cmd.ExecuteScalar().ToString
-            Label_Details.Text = name
+
+            While (Reader.Read())
+                name = Reader.GetValue(1)
+                dept = Reader.GetValue(2)
+                email = Reader.GetValue(3)
+                PictureBox2.Image = Image.FromFile(Reader.GetValue(4))
+            End While
             conn.Close()
         Catch ex As Exception
             MessageBox.Show(ex.Message, "Warning")
         End Try
 
-
+        Label_Details.Text = name & vbNewLine & dept & vbNewLine & email
 
 
 
