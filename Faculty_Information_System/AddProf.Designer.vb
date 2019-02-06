4aa0b21b10dfcae4d57a83fae6ee492d7f8c3a3b
Aryan <aryanpranshu@gmail.com>
2019-02-07 00:09:26 +0530

    Add faculty option for admin added

--- Faculty_Information_System/AddProf.Designer.vb
+++ Faculty_Information_System/AddProf.Designer.vb
@@ -47,36 +47,40 @@ Partial Class AddProf
         'NameLabel
         '
         NameLabel.AutoSize = True
-        NameLabel.Location = New System.Drawing.Point(79, 42)
+        NameLabel.Location = New System.Drawing.Point(59, 34)
+        NameLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         NameLabel.Name = "NameLabel"
-        NameLabel.Size = New System.Drawing.Size(49, 17)
+        NameLabel.Size = New System.Drawing.Size(38, 13)
         NameLabel.TabIndex = 1
         NameLabel.Text = "Name:"
         '
         'DepartmentLabel
         '
         DepartmentLabel.AutoSize = True
-        DepartmentLabel.Location = New System.Drawing.Point(79, 85)
+        DepartmentLabel.Location = New System.Drawing.Point(59, 69)
+        DepartmentLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         DepartmentLabel.Name = "DepartmentLabel"
-        DepartmentLabel.Size = New System.Drawing.Size(86, 17)
+        DepartmentLabel.Size = New System.Drawing.Size(65, 13)
         DepartmentLabel.TabIndex = 3
         DepartmentLabel.Text = "Department:"
         '
         'EmailLabel
         '
         EmailLabel.AutoSize = True
-        EmailLabel.Location = New System.Drawing.Point(82, 138)
+        EmailLabel.Location = New System.Drawing.Point(62, 112)
+        EmailLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         EmailLabel.Name = "EmailLabel"
-        EmailLabel.Size = New System.Drawing.Size(46, 17)
+        EmailLabel.Size = New System.Drawing.Size(35, 13)
         EmailLabel.TabIndex = 5
         EmailLabel.Text = "Email:"
         '
         'PasswordLabel
         '
         PasswordLabel.AutoSize = True
-        PasswordLabel.Location = New System.Drawing.Point(79, 188)
+        PasswordLabel.Location = New System.Drawing.Point(59, 153)
+        PasswordLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         PasswordLabel.Name = "PasswordLabel"
-        PasswordLabel.Size = New System.Drawing.Size(73, 17)
+        PasswordLabel.Size = New System.Drawing.Size(56, 13)
         PasswordLabel.TabIndex = 7
         PasswordLabel.Text = "Password:"
         '
@@ -103,49 +107,54 @@ Partial Class AddProf
         'TextBox1
         '
         Me.TextBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Faculty_infoBindingSource, "Name", True))
-        Me.TextBox1.Location = New System.Drawing.Point(188, 42)
+        Me.TextBox1.Location = New System.Drawing.Point(141, 34)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(76, 20)
         Me.TextBox1.TabIndex = 2
         '
         'TextBox2
         '
         Me.TextBox2.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Faculty_infoBindingSource, "Department", True))
-        Me.TextBox2.Location = New System.Drawing.Point(188, 85)
+        Me.TextBox2.Location = New System.Drawing.Point(141, 69)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.Size = New System.Drawing.Size(76, 20)
         Me.TextBox2.TabIndex = 4
         '
         'TextBox3
         '
         Me.TextBox3.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Faculty_infoBindingSource, "Email", True))
-        Me.TextBox3.Location = New System.Drawing.Point(188, 138)
+        Me.TextBox3.Location = New System.Drawing.Point(141, 112)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.Size = New System.Drawing.Size(76, 20)
         Me.TextBox3.TabIndex = 6
         '
         'TextBox4
         '
         Me.TextBox4.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.Faculty_infoBindingSource, "Password", True))
-        Me.TextBox4.Location = New System.Drawing.Point(188, 185)
+        Me.TextBox4.Location = New System.Drawing.Point(141, 150)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.Size = New System.Drawing.Size(76, 20)
         Me.TextBox4.TabIndex = 8
         '
         'Update
         '
-        Me.Update.Location = New System.Drawing.Point(249, 270)
+        Me.Update.Location = New System.Drawing.Point(187, 219)
+        Me.Update.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Update.Name = "Update"
-        Me.Update.Size = New System.Drawing.Size(112, 32)
+        Me.Update.Size = New System.Drawing.Size(84, 26)
         Me.Update.TabIndex = 9
         Me.Update.Text = "Update"
         Me.Update.UseVisualStyleBackColor = True
         '
         'AddProf
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(589, 352)
+        Me.ClientSize = New System.Drawing.Size(442, 286)
         Me.Controls.Add(Me.Update)
         Me.Controls.Add(PasswordLabel)
         Me.Controls.Add(Me.TextBox4)
@@ -155,6 +164,7 @@ Partial Class AddProf
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(NameLabel)
         Me.Controls.Add(Me.TextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "AddProf"
         Me.Text = "AddProf"
         CType(Me.Faculty_databaseDataSet, System.ComponentModel.ISupportInitialize).EndInit()
--- Faculty_Information_System/AddProf.vb
+++ Faculty_Information_System/AddProf.vb
@@ -5,17 +5,17 @@ Imports System.Data
 Public Class AddProf
     Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Faculty_database.accdb;Jet OLEDB:Database Password=group11"
 
-    Private Sub Update_button_Click(sender As Object, e As EventArgs)
+    Private Sub Update_Click(sender As Object, e As EventArgs) Handles Update.Click
         Dim conn = New OleDbConnection(connectionString)
-            conn.Open()
-            If String.IsNullOrEmpty(TextBox1.Text) Or String.IsNullOrEmpty(TextBox2.Text) Or String.IsNullOrEmpty(TextBox3.Text) Or String.IsNullOrEmpty(TextBox4.Text) Then
-                MessageBox.Show("First Fill all the required entries")
-                Exit Sub
-            End If
-            Dim namestring As String = TextBox1.Text
-            Dim emailstring As String = TextBox2.Text
-            Dim departmentstring As String = TextBox3.Text
-            Dim passwordstring As String = TextBox4.Text
+        conn.Open()
+        If String.IsNullOrEmpty(TextBox1.Text) Or String.IsNullOrEmpty(TextBox2.Text) Or String.IsNullOrEmpty(TextBox3.Text) Or String.IsNullOrEmpty(TextBox4.Text) Then
+            MessageBox.Show("First Fill all the required entries")
+            Exit Sub
+        End If
+        Dim namestring As String = TextBox1.Text
+        Dim emailstring As String = TextBox2.Text
+        Dim departmentstring As String = TextBox3.Text
+        Dim passwordstring As String = TextBox4.Text
         Dim query As String
         query = "INSERT INTO faculty_info ([Name],[Department],[Email],[Password]) VALUES (?,?,?,?)"
         Dim cmd As OleDbCommand = New OleDbCommand(query, conn)
@@ -23,14 +23,16 @@ Public Class AddProf
         cmd.Parameters.Add(New OleDbParameter("Email", CType(emailstring, String)))
         cmd.Parameters.Add(New OleDbParameter("Department", CType(departmentstring, String)))
         cmd.Parameters.Add(New OleDbParameter("Password", CType(passwordstring, String)))
+
+
         Try
             cmd.ExecuteNonQuery() 'Executing Update Command
             cmd.Dispose()
         Catch ex As Exception
             MessageBox.Show(ex.Message) 'Error MEssage
         End Try
+        Me.Hide()
+        Admin_form.Show()
         conn.Close()
     End Sub
-
-
 End Class
