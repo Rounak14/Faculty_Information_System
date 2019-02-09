7454757b0fad5b639d623a4d981d5fffe76ea89e
priyanshu2103 <priyanshu.singh2k@gmail.com>
2019-02-10 02:04:45 +0530

    delete prof added

--- Faculty_Information_System/AddProf.vb
+++ Faculty_Information_System/AddProf.vb
@@ -43,4 +43,8 @@ Public Class AddProf
         Me.Close()
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Admin_form.Show()
+        Me.Close()
+    End Sub
 End Class
---
+++ Faculty_Information_System/DelProf.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.OleDb
+Imports System.Data
+
+Public Class DelProf
+    Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Faculty_database.accdb;Jet OLEDB:Database Password=group11"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If String.IsNullOrEmpty(TextBox1.Text) Then
+            MessageBox.Show("email entry must not be empty", "Warning")
+            TextBox1.Clear()
+            Exit Sub
+        End If
+
+        Dim conn = New OleDbConnection(connectionString)
+        Dim EmailPass = TextBox1.Text
+        Dim query As String = "delete from faculty_info where Email =@sno"
+        conn.Open()
+        Dim cmd As New OleDbCommand(query, conn)
+        cmd.Parameters.AddWithValue("@sno", EmailPass)
+
+        Try
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        conn.Close()
+        Admin_form.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Admin_form.Show()
+        Me.Close()
+    End Sub
+End Class
